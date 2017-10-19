ec619c963d6ca21168556155c02fd0d6e3378857
mjl1010 <mjl1010@gmx.es>
2017-10-19 21:18:15 +0200

    Implements Fragment

--- app/src/main/java/com/skimdoo/dynamicfragmentsdemo3/MainActivity.java
+++ app/src/main/java/com/skimdoo/dynamicfragmentsdemo3/MainActivity.java
@@ -1,13 +1,19 @@
 package com.skimdoo.dynamicfragmentsdemo3;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
     }
 }
